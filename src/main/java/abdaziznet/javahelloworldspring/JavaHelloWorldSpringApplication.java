ee1d9c422f3698dc5a49246565cec00a38e9976a
Abdul Aziz <dev.abdaziz@outlook.com>
2022-07-22 11:44:40 +0700

    first commit

---
+++ src/main/java/abdaziznet/javahelloworldspring/JavaHelloWorldSpringApplication.java
@@ -0,0 +1,13 @@
+package abdaziznet.javahelloworldspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHelloWorldSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHelloWorldSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/abdaziznet/javahelloworldspring/JavaHelloWorldSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package abdaziznet.javahelloworldspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHelloWorldSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
